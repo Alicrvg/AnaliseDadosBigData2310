8f4340c8594eede7608970d712f6d800bd009fd4
Alicrvg <aliceolive85@gmail.com>
2026-03-09 21:04:51 -0300

    adiciona material aula01

---
+++ Aula01/aula01-02032026.py
@@ -0,0 +1,45 @@
+##AULA 01 - VARIAVEIS
+
+##Tipos de Dados
+
+
+print('olá mundo')
+
+nome='maria'
+idade=30
+preco=19.99
+print(type(preco))
+decisao1=True 
+decisao2=False 
+print(decisao1)
+print(print(decisao1))
+
+
+##ATIVIDADE 04/03/2026
+##BOLETIM
+
+nota1=8
+nota2=6
+media=(nota1+nota2)/2
+print(media)
+
+##CALCULADORA
+soma=6+8
+subtracao=9-7
+multiplicacao=8*5
+divisao=40/5
+print(soma)
+print(subtracao)
+print(multiplicacao)
+print(divisao)
+
+##correção calculadora
+numero1=3
+numero2=4
+soma=numero1+numero2
+subtração=numero1-numero2
+multiplicacao=numero1*numero2
+divisao=numero1/numero2
+modulo=numero1%numero2
+
+print(soma,subtracao,multiplicacao,divisao,modulo)
