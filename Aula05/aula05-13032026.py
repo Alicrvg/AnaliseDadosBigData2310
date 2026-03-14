ff0a59c1ce7d6fa40722e5ccb243c6d2418ab92f
Alicrvg <aliceolive85@gmail.com>
2026-03-13 21:53:15 -0300

    adiciona a pasta

---
+++ Aula05/aula05-13032026.py
@@ -0,0 +1,32 @@
+
+ #antes de começar qualque laço, tem que declarar a variavel
+# loolap=float(input('Qual o valor do ingresso'))
+
+# while loolap > 300: 
+#     print("mas não vou mesmo")
+# print("estarei la")
+
+# loolao=0
+# while loolap > 300: 
+#     loolap=float(input('Qual o valor do ingresso'))
+#     print("mas não vou mesmo")
+# print("estarei la")
+
+#LISTAS:
+#indices:   0        1       2        3        4
+nomes=['Matheus','Alice','Caio','Larissa','Miguel','Rafael']
+print(nomes[1])
+# de tras pra frente a contagem começa do "-1"
+print(nomes[-1])
+print(nomes[-2])
+print(nomes[-3])
+print(nomes[-4])
+print(nomes[-5])
+
+primeira_parte=nomes[0:2]
+primeira_parte=nomes[0:3]
+print(primeira_parte)
+#ultimo numero é sempre descartado!!!!!!!!
+# ":" faz um intervalo
+
+print(len(nomes))
